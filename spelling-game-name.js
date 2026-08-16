e0e4501bf565b3b8150ca6b2a06903359c2f6fba
limkimsze-maker <lim_kim_sze@moe.edu.sg>
2026-08-16 12:48:48 +0800

    Rename Phonogram Challenge to Spelling Game

---
+++ spelling-game-name.js
@@ -0,0 +1,4 @@
+  // Use the clearer public-facing name throughout the game.
+  document.title='Spelling Game';
+  const mainGameTitle=document.querySelector('h1');
+  if(mainGameTitle) mainGameTitle.textContent='Spelling Game';
